a03388d07de165a18c1cb67bf3377d9897e1b152
Ethan Weygang <ethanweygang@gmail.com>
2025-01-27 18:21:04 +0000

    lol

--- app/main.py
+++ app/main.py
@@ -466,7 +466,7 @@ class Interpreter:
     def IsTruthy(self, obj):
         if obj == None: return "false"
         if isinstance(obj, bool): return "true" if obj.value == True else "false" #so messy dont think this line is needed
-        if obj.value == "false":
+        if obj == "false":
             return "true"
 
     def Evaluate(self, expr):
